40c5e2926ee014e327e2d25bfbee756bbd87dcc5
umesh-dahal <80223504+umesh-dahal@users.noreply.github.com>
2024-04-29 16:15:02 +0200

    Delete Website directory

--- Website/static/js/nav.js
+++
@@ -1,5 +0,0 @@
-// JavaScript code to show the navigation after 3 seconds
-setTimeout(function() {
-    document.getElementById('main-nav').style.display = 'block';
-  }, 4500); // 3000 milliseconds = 3 seconds
-  
